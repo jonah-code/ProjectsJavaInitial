68a002d3593a9f75e7a4222dfe79c16b9563199e
jonas <jonas.silva.087@gmail.com>
2022-06-16 14:56:56 -0300

    addition project bar version 1

--- src/ProjetoComClasse/ProjetoBar/Bar.java
+++ src/ProjetoComClasse/ProjetoBar/Bar.java
@@ -1,5 +1,41 @@
 package ProjetoComClasse.ProjetoBar;
 
-public class Bar {
+import java.util.Locale;
+import java.util.Scanner;
 
+public class Bar {
+	public static void main(String[] args) {
+		
+		Bill bill = new Bill();
+		char gender;
+		int qtBeer;
+		int qtBarbecue;
+		int qtSoftDrink;
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("sexo:");
+		gender = sc.next().charAt(0);
+		bill.gender = gender;
+		
+		
+		System.out.print("quantidade de cerveja:");
+		qtBeer = sc.nextInt();
+		bill.beer = qtBeer;
+		
+		System.out.print("quantidade de refrigerantes:");
+		qtSoftDrink = sc.nextInt();
+		bill.softDrinks = qtSoftDrink;
+		
+		
+		System.out.print("quantidade de churasco:");
+		qtBarbecue = sc.nextInt();
+		bill.barbecue =  qtBarbecue;
+		
+		
+		System.out.println(bill);
+		//System.out.printf("%s %d %d", gender, qtBeer , qtBarbecue);
+		sc.close();
+	}
 }
